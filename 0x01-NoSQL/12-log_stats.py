84d9e719afaad6188ae291cadd9f7c9412e578e1
Virgo Alpha <bensonkingori96@gmail.com>
2022-06-14 11:12:23 +0300

    het

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
+"""
+Module 12-log_stats.py
+"""
 from pymongo import MongoClient
-"""
-A python module that provides some stats about Nginx logs
-"""
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    log_collection = client.logs.nginx
-    nbr_logs = log_collection.count_documents({})
-    print("{} logs".format(nbr_logs))
+    nginx_collection = client.logs.nginx
+
+    total_logs = nginx_collection.count_documents({})
+    total_gets = nginx_collection.count_documents({"method": "GET"})
+    total_posts = nginx_collection.count_documents({"method": "POST"})
+    total_puts = nginx_collection.count_documents({"method": "PUT"})
+    total_patchs = nginx_collection.count_documents({"method": "PATCH"})
+    total_deletes = nginx_collection.count_documents({"method": "DELETE"})
+    total_status = nginx_collection.count_documents(
+                    {"method": "GET", "path": "/status"})
+
+    print("{} logs".format(total_logs))
     print("Methods:")
-    for mtd in methods:
-        print("\tmethod {} {}".format(
-            mtd, log_collection.count_documents({"methods": mtd})
-        ))
-    nbr_checks = log_collection.count_documents(
-        {"methods": "GET", "path": "/status"})
-    print("{} status check".format(nbr_checks))
+    print("\tmethod GET: {}".format(total_gets))
+    print("\tmethod POST: {}".format(total_posts))
+    print("\tmethod PUT: {}".format(total_puts))
+    print("\tmethod PATCH: {}".format(total_patchs))
+    print("\tmethod DELETE: {}".format(total_deletes))
+    print("{} status check".format(total_status))
