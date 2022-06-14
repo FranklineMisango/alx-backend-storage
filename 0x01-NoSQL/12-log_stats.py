3c4b0acfc48f7afee020f655e4963fe002f43d52
Virgo Alpha <bensonkingori96@gmail.com>
2022-06-14 11:08:11 +0300

    store

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from pymongo import MongoClient
-""" a python module that provides some stats about Nginx logs"""
+"""
+A python module that provides some stats about Nginx logs
+"""
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
